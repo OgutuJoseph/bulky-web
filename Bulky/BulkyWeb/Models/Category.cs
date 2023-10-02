8794bce3587155a99f76132276005fe85d6a13db
OgutuJoseph <onyango.joseph@yahoo.com>
2023-10-02 15:42:04 +0300

    Section 2: Data Annotations

--- Bulky/BulkyWeb/Models/Category.cs
+++ Bulky/BulkyWeb/Models/Category.cs
@@ -1,7 +1,10 @@
-﻿namespace BulkyWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
 {
 	public class Category
 	{
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
