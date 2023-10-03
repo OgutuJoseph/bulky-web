90b64b5beca29d330c37435e4a65d6013fb8faa3
OgutuJoseph <onyango.joseph@yahoo.com>
2023-10-03 12:34:59 +0300

    Section 2: Server Side Validations

--- Bulky/BulkyWeb/Models/Category.cs
+++ Bulky/BulkyWeb/Models/Category.cs
@@ -9,8 +9,10 @@ namespace BulkyWeb.Models
         public int Id { get; set; }
         [Required]
         [DisplayName("Category Name")]
+        [MaxLength(30)]
         public string Name { get; set; }
         [DisplayName("Display Order")]
+        [Range(1, 2, ErrorMessage = "Display Order must be between 1 and 2")]
         public int DisplayOrder { get; set; }
     }
 }
