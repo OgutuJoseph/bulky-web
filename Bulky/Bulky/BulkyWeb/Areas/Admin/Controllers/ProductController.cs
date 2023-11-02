955db4aef42b3b78222b923817ca950f557cbbe9
OgutuJoseph <onyango.joseph@yahoo.com>
2023-11-02 20:26:04 +0300

    Section 6: View Models in Action

--- Bulky/Bulky/BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ Bulky/Bulky/BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections;
@@ -23,28 +24,34 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
 
         public IActionResult Create()
         {
-            IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
-            {
-                Text = u.Name,
-                Value = u.Id.ToString()
-            });
+            //IEnumerable<SelectListItem> CategoryList = _unitOfWork...
 
             //ViewBag.CategoryList = CategoryList;
-            ViewData["Categories"] = CategoryList;
+            //ViewData["Categories"] = CategoryList;
+            ProductVM productVM = new()
+            {
+                //CategoryList = CategoryList,
+                CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }),
+                Product = new Product()
+            };
 
-            return View();
+            return View(productVM);
         }
 
         [HttpPost]
-        public IActionResult Create(Product obj)
+        public IActionResult Create(ProductVM obj)
         {
-            if (obj.Title != null && obj.Title.ToLower() == "test" || obj.Title != null && obj.Title.ToLower() == "testing")
-            {
-                ModelState.AddModelError("", "Test is an invalid value");
-            }
+            //if (obj.Product.Title != null && obj.Product.Title.ToLower() == "test" || obj.Product.Title != null && obj.Product.Title.ToLower() == "testing")
+            //{
+            //    ModelState.AddModelError("", "Test is an invalid value");
+            //}
             if (ModelState.IsValid)
             {
-                _unitOfWork.Product.Add(obj);
+                _unitOfWork.Product.Add(obj.Product);
                 _unitOfWork.Save();
                 TempData["success"] = "Product created successfully.";
                 return RedirectToAction("Index");
