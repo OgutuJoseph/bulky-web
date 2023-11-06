d2569d6441e9aa764ca591fbf1fcddfc52451034
OgutuJoseph <onyango.joseph@yahoo.com>
2023-11-06 13:22:34 +0300

    Section 6: Edit Product Link in DataTable

--- Bulky/Bulky/BulkyWeb/wwwroot/js/product.js
+++ Bulky/Bulky/BulkyWeb/wwwroot/js/product.js
@@ -11,7 +11,17 @@ function loadDataTable() {
             { data: 'author', "width": "15%" },
             { data: 'listPrice', "width": "15%" },
             { data: 'price', "width": "10%" },
-            { data: 'category.name', "width": "10%" }
+            { data: 'category.name', "width": "10%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                        <a href="/admin/product/upsert?id=${data}" class="btn btn-info mx-2"><i class="bi bi-pencil-square"></i></a>
+                        <a href="/admin/product/delete?id=${data}" class="btn btn-danger mx-2"><i class="bi bi-trash-fill"></i></a>
+                    </div >`
+                },
+                "width": "15%"
+            }
         ]
     });
 };
