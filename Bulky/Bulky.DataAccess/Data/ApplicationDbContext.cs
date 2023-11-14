c89c48d50ada49bcb1d943af2cdcaaf18ec2c2b5
OgutuJoseph <onyango.joseph@yahoo.com>
2023-11-14 10:01:28 +0300

    Section 11: Create Order Header and Details Model

--- Bulky/Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky/Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -22,6 +22,10 @@ namespace BulkyBook.DataAccess.Data
 
         public DbSet<ShoppingCart> ShoppingCarts { get; set; }
 
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
