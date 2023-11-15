d513b23be69632df22641d56fbea9569fd0093cc
OgutuJoseph <onyango.joseph@yahoo.com>
2023-11-15 15:41:14 +0300

    Section 12: Order List UI

---
+++ Bulky/BulkyWeb/wwwroot/js/order.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#ordersTable').DataTable({
+        "ajax": { url: '/admin/order/getall' },
+        "columns": [
+            { data: 'id' },
+            { data: 'name' },
+            { data: 'phoneNumber' },
+            { data: 'applicationUser.email' },
+            { data: 'orderStatus' },
+            { data: 'orderTotal' },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                        <a href="/admin/product/details?id=${data}" class="btn btn-info mx-2"><i class="bi bi-eye"></i></a>
+                     </div >`
+                },
+                "width": "15%"
+            }
+        ]
+    });
+};
