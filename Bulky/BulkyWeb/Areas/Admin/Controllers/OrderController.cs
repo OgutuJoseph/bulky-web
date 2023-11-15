06494daa11e8301048d58e454271259bd55b883f
OgutuJoseph <onyango.joseph@yahoo.com>
2023-11-15 14:34:39 +0300

    Section 12: OrderVM and Order Controller

---
+++ Bulky/BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,40 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	[Authorize(Roles = SD.Role_Admin)]
+	public class OrderController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+
+		public OrderController(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+
+		#region Action Methods
+		public IActionResult Index()
+		{
+			return View();
+		}
+		#endregion
+
+		#region API Calls
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+			return Json(new
+			{
+				data = objOrderHeaders
+			});
+		}
+		#endregion
+	}
+}
